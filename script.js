6b1266143225007ea3286b7068f5adef71f44d24
arakhor <csmolenski23@gmail.com>
2024-01-03 15:01:25 +0100

    ff

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const purchaseBtnEl = document.getElementById("purchase-btn");
 const changeDue = document.getElementById("change-due").value;
 
 let price = 23.5;
-let cid = [
+const cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
   ["DIME", 3.1],
@@ -14,23 +14,36 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
 ];
-const cidReduced = cid.reduce((a, b) => a + b[1], 0);
+// let cash = document.getElementById("cash").value;
+// const cidReduced = cid.reduce((a, b) => a + b[1], 0);
+let cash = 100;
 
-let status = `Status: OPEN TWENTY: $60 TEN: $20 FIVE: $15 ONE: $1 QUARTER: $0.5 DIME: $0.2 PENNY: $0.04`;
+function getNewCid(change) {
+  const lookup = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
 
-function getChange() {
-  const lookup = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  let cash = document.getElementById("cash").value;
-  cid.forEach((x) => {
-    console.log(x)
-  })
+  let newCid = cid.slice();
+
+  for (let i = newCid.length - 1; i >= 0; i--) {
+    while (newCid[i][1] >= lookup[i] && change >= lookup[i]) {
+      newCid[i][1] -= lookup[i];
+      change -= lookup[i];
+    }
+  }
+  return newCid;
+}
+
+function getChangeStr() {
+  const change = cash - price;
+  let changeStr = `Status: OPEN\n`;
+  const newCid = getNewCid(change);
+  console.log(cid);
 }
 
 purchaseBtnEl.addEventListener("click", () => {
   if (cash < price) {
     alert("Customer does not have enough money to purchase the item");
-  } else if (cash === price) {
-    alert("No change due - customer paid with exact cash");
+    // } else if (cash === price) {
+    // alert("No change due - customer paid with exact cash");
   } else {
     getChange();
   }
