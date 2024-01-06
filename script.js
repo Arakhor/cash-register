6ad4c33ac1c515a860fd5e619bea36d606533056
arakhor <csmolenski23@gmail.com>
2024-01-06 19:29:51 +0100

    everything works but test is bugged

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const purchaseBtnEl = document.getElementById("purchase-btn");
 const changeDueEl = document.getElementById("change-due");
 const cashEl = document.getElementById("cash");
 
-let price = 3.26;
+let price = 19.5;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
+  ["PENNY", 0.5],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 0],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0],
 ];
+
+function substractFloat(a, b) {
+  return (a * 1000 - b * 1000) / 1000;
+}
 
 purchaseBtnEl.addEventListener("click", () => {
   let cash = cashEl.value * 1;
@@ -32,10 +36,9 @@ purchaseBtnEl.addEventListener("click", () => {
   const lookup = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
   for (let i = cid.length - 1; i >= 0; i--) {
     while (changeDue >= lookup[i] && cid[i][1] >= lookup[i]) {
-      console.log(changeDue);
       change[i][1]++;
-      cid[i][1] -= lookup[i];
-      changeDue -= lookup[i];
+      cid[i][1] = substractFloat(cid[i][1], lookup[i]);
+      changeDue = substractFloat(changeDue, lookup[i]);
     }
   }
 
@@ -49,13 +52,23 @@ purchaseBtnEl.addEventListener("click", () => {
     return bool;
   };
 
-  const printChange = () => {
+  const printChange = (status) => {
     let changeString = ``;
-    change.forEach((x, i) => {
-      if (x[1] > 0) {
-        changeString += `${x[0]}: $${x[1] * lookup[i]}\n`;
-      }
-    });
+    switch (status) {
+      case "open":
+        change.forEach((x, i) => {
+          if (x[1] > 0) {
+            changeString += `${x[0]}: $${x[1] * lookup[i]}\n`;
+          }
+        });
+        break;
+      case "closed":
+        for (let i = 3; i >= 0; i--) {
+          changeString += `${change[i][0]}: $${change[i][1] * lookup[i]}\n`;
+        }
+        break;
+    }
+
     return changeString;
   };
 
@@ -66,8 +79,8 @@ purchaseBtnEl.addEventListener("click", () => {
   } else if (changeDue > 0) {
     changeDueEl.innerText = `Status: INSUFFICIENT_FUNDS`;
   } else if (isDrawerEmpty()) {
-    changeDueEl.innerText = `Status: CLOSED`;
+    changeDueEl.innerText = `Status: CLOSED \n${printChange("closed")}`;
   } else {
-    changeDueEl.innerText = `Status: OPEN \n${printChange()}`;
+    changeDueEl.innerText = `Status: OPEN \n${printChange("open")}`;
   }
 });
